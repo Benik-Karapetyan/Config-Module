cd847d9fb3f2dec2d0b57e76fd6a5e5da8f4fdc9
David Hovhannisyan <davith@redkite.io>
2021-04-06 17:12:52 +0400

    adding .env support, webpack config with craco, ant design ui lib.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Input} from "antd";
 
-function App() {
+const App = () => {
+  const [input, setInput] = React.useState('');
+
+  React.useEffect(() => {
+    console.log('effect')
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> {input} and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+
+        <Button type="primary">
+          Button
+        </Button>
       </header>
     </div>
   );
---
+++ src/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  withCredentials: false,
+  baseURL: process.env.REACT_APP_API_URL
+});
+
+api.interceptors.request.use((req) => {
+  return req;
+});
+
+api.interceptors.response.use((res) => {
+  return res;
+});
+
+export default api;
